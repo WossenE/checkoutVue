c1acc74c7f4bb7a1638fe251737d4302a6e4026b
Wossen Hailemariam <wossenhailemariam@Wossens-MacBook-Pro.local>
2023-01-22 22:52:07 +0100

    last changes

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -14,25 +14,26 @@ export default {
 
 <style scoped>
 .button {
-  background-color: #9bbf9c;
+  background-color: #4caf50; /* Green */
   border: none;
   color: white;
-  padding: 5px 32px;
+  padding: 5px 16px;
   text-align: center;
   text-decoration: none;
   display: inline-block;
   font-size: 26px;
+  font-weight: 800;
   margin-bottom: 0.5em;
   border-radius: 25px;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 }
 
 .button {
-  transition-duration: 0.7s;
+  transition-duration: 0.5s;
 }
 
 .button:hover {
-  background-color: #4CAF50; /* Green */
+  background-color: #9bbf9c;
   color: white;
 }
 </style>
